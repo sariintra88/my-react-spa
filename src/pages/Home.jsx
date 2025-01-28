c41929c0ddad578f6ec30d485ac6b69e0ba662c1
sariintra88 <sarintra.sir@dome.tu.ac.th>
2025-01-28 12:02:59 +0700

    update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {  //const คือบังคับไม่เปลี่ยน
+    return (
+      <div>
+        <h1>ยินดีต้อนรับสู่ SPA ของฉัน</h1>
+        <p>นี่คือหน้าแรก</p>
+      </div>
+    )
+  }
+  
+  export default Home
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-react-spa/', // แทนที่ด้วยชื่อ repository ของตัวเอง
 })
